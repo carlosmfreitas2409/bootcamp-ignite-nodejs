e0e86308bb97b6aa048de4bc2890d590369532a9
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2022-11-09 21:45:37 -0300

    chore: move challenges repositories to monorepo

---
+++ challenges/02-introducao-ao-SOLID/src/modules/users/model/User.ts
@@ -0,0 +1,27 @@
+import { v4 as uuid } from 'uuid';
+
+class User {
+  id: string;
+
+  name: string;
+
+  email: string;
+
+  admin: boolean;
+
+  created_at: Date;
+
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+
+    if (!this.admin) {
+      this.admin = false;
+    }
+  }
+}
+
+export { User };
